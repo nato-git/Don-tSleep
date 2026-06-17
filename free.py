01fe29a4ef74f7c484ed8658ddaac20fac5aac49
nato-git <s.f.h.o1219@gmail.com>
2026-06-17 21:36:21 +0900

    READMEの記入

--- free.py
+++ free.py
@@ -7,11 +7,22 @@ import os
 
 model = YOLO("yolo11n-pose.pt")
 pr3_cap = cv2.VideoCapture(0)
+# 寝ているかどうかのフラグ
 flag = False
+
+# 壁紙を変更するためのパス
 path = ""
 SPI_SETDESKWALLPAPER = 20
 SPIF_UPDATEINIFILE = 1
 SPIF_SENDCHANGE = 2
+
+# 元の壁紙を取得して保存
+SPI_GETDESKWALLPAPER = 115
+buffer = ctypes.create_unicode_buffer(260)
+now_wallpaper_path = ctypes.windll.user32.SystemParametersInfoW(
+    SPI_GETDESKWALLPAPER, len(buffer), buffer, 0)
+now_wallpaper = cv2.imread(buffer.value)
+cv2.imwrite("WriteImage/now_wallpaper.jpg", now_wallpaper)
 
 while True:
   ret, frame = pr3_cap.read()
@@ -19,15 +30,21 @@ while True:
     results = model.track(frame, persist=True)
     keypoints = results[0].keypoints
     if keypoints is not None:
+      # 体の部位を取得
       for kpts in keypoints.xy:
+        # 右耳
         right_ear = kpts[4]
+        # 左耳
         left_ear = kpts[3]
+        # 鼻
         nose = kpts[0]
+        # 右耳の座標が取得できた場合に角度を計算
         if right_ear[0] > 0 and right_ear[1] > 0:
           dx = float(right_ear[0]) - float(left_ear[0])
           dy = float(right_ear[1]) - float(left_ear[1])
           angle = np.degrees(np.arctan2(dy, dx))
           print(f"頭の傾き: {angle:.2f}度")
+          # 頭の傾きが25度以上の場合は寝ていると判断
           if 180 - abs(angle) > 25:
             print("寝ないでください")
             flag = True
@@ -35,14 +52,18 @@ while True:
             flag = False
     frame_track = results[0].plot()
     if flag:
+      # 矢印
       cv2.arrowedLine(frame_track, (int(nose[0]), max(int(
           nose[1]) - 400, 0)), (int(nose[0]), max(int(nose[1]) - 200, 0)), (0, 0, 255), thickness=10)
+      # Don't sleep!
       cv2.putText(frame_track, "Don't sleep!", (10, 30),
                   cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+      # カメラ映像のコピー
       sleep_track = frame_track.copy()
-      points = np.array([0, 0], dtype=np.int32)
+      # コピーに現在時刻を表示
       cv2.putText(sleep_track, f"{datetime.datetime.now()}", (10, 60),
                   cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+      # コピーを保存して壁紙に設定
       cv2.imwrite("sleep_face.jpg", sleep_track)
       path = os.path.abspath("sleep_face.jpg")
       ctypes.windll.user32.SystemParametersInfoW(
